04a4e15c6150117fb2b92c8e92501cb5f13bac86
Iovi Molander <iovmo629@student.liu.se>
2020-02-01 17:55:06 +0100

    WaypointManager, Waypoint, CarWaypoint

---
+++ Assets/Scripts/CarWaypoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWaypoint : MonoBehaviour
+{
+    int currentWaypoint;
+    WaypointManager myWaypointManager;
+
+    //When the Primitive collides with the walls, it will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == Waypoint){
+            currentWayPoint = myWaypointManager.getNextWaypoint();
+        }
+        if(currentWaypoint == NULL){
+            //Desummon + PlayerScore+1
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWaypoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    List[] WaypointList;
+    int max;
+    int getNextWaypoint(){
+        CarWaypoint car = GameObject.GetComponent("CarWaypoint") as CarWaypoint;
+        if(car.currentWaypoint == (max-1)){
+            return null;
+        }
+        else{
+            return (car.currentWaypoint + 1);
+            
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
