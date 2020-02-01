05157d77daa0bab85d8f5ded274cff9321b13191
Jesper Tingvall <jesper.tingvall@hiq.se>
2020-02-01 21:59:20 +0100

    Add Death and win working UI and bridge thing

---
+++ Assets/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour {
+
+    public Transform Pos;
+    public GameObject Player;
+
+    public void Restart() {
+        Application.LoadLevel(0);
+    }
+
+    public void TeleportToStart() {
+        Player.transform.position = Pos.position;
+        Player.transform.rotation = Pos.rotation;
+    }
+}
