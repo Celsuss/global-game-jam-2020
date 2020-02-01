8381463be76b5c531e793288d4270043563ee9cb
Celsuss <jens.lorden@imagimob.com>
2020-02-01 19:13:46 +0100

    Trying to fix

---
+++ Assets/Scripts/Waypoints/CarWaypoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CarWaypoint : MonoBehaviour
+{
+    [SerializeField]
+    WaypointManager m_WaypointManager;
+    int m_CurrentWaypointIndex;
+    Transform m_CurrentWaypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_CurrentWaypointIndex = 0;
+        m_CurrentWaypoint = null;
+        Assert.IsNotNull(m_WaypointManager);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Waypoint")
+        {    
+            Debug.Log("Waypoint reached");
+            m_CurrentWaypoint = m_WaypointManager.GetNextWaypoint(m_CurrentWaypointIndex);
+            m_CurrentWaypointIndex += 1;
+
+            if(m_CurrentWaypoint == null)
+            {
+                // Kill the car
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoints/WaypointManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> m_Waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Transform GetNextWaypoint(int currentWaypointIndex)
+    {
+        if(currentWaypointIndex >= m_Waypoints.Count)
+            return null;
+
+        return m_Waypoints[currentWaypointIndex+1]; 
+    }
+}
