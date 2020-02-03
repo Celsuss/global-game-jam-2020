05edd7775ddcef44f83b362bc9113c5accc75263
Jesper Tingvall <jesper@tingvall.pw>
2020-02-03 09:34:30 +0100

    Clean up controlls. Set other itchio project page. Add project icon.

--- Assets/UnityBuild/BuildConstants.cs
+++ Assets/UnityBuild/BuildConstants.cs
@@ -5,19 +5,25 @@ public static class BuildConstants
     public enum ReleaseType
     {
         None,
-        Itch_io,
+        DRM_Free,
     }
 
     public enum Platform
     {
         None,
         WebGL,
+        PC,
+        Linux,
+        OSX,
     }
 
     public enum Architecture
     {
         None,
         WebGL,
+        Windows_x86,
+        Linux_x64,
+        OSX,
     }
 
     public enum Distribution
@@ -25,11 +31,11 @@ public static class BuildConstants
         None,
     }
 
-    public static readonly System.DateTime buildDate = new System.DateTime(636935460353115974);
-    public const string version = "0.4.16";
-    public const ReleaseType releaseType = ReleaseType.Itch_io;
-    public const Platform platform = Platform.WebGL;
-    public const Architecture architecture = Architecture.WebGL;
+    public static readonly System.DateTime buildDate = new System.DateTime(637163098826433107);
+    public const string version = "1.0.1";
+    public const ReleaseType releaseType = ReleaseType.DRM_Free;
+    public const Platform platform = Platform.Linux;
+    public const Architecture architecture = Architecture.Linux_x64;
     public const Distribution distribution = Distribution.None;
 }
 
